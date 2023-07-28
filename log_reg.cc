f3993449cf97fd4796d7f8169decc3f552e0443b
R-Rohith <92912027+R-Rohith@users.noreply.github.com>
2023-07-28 21:33:39 +0530

    Add files via upload

---
+++ log_reg.cc
@@ -0,0 +1,8 @@
+#include<iostream>
+int main()
+{                       //logistic regression...what kind of data do we need....
+    float x1[],x2[],y[];
+    //I guess its just similar to linear...just a different kind of function.
+    //We'll do Neural networks then....
+    return 0;
+}
