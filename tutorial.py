acdf44d044254b63d7b8986061bcbff6149c18b1
R-Rohith <rohit120502@gmail.com>
2024-08-28 12:08:26 +0530

    python_tutorial

---
+++ tutorial.py
@@ -0,0 +1,10 @@
+print('Hello World')
+a=10
+b='hi there'
+c='hey there!'
+print(b,a,c)
+print(b+c)
+name=input("Who are you? ")
+print(c,name)
+age=int(input("How old are you? "))
+print("Right, so you are",name+", age:",age,"\byrs")
